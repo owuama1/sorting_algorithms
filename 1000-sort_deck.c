368731cfe52a18b5c27dc85befac632627a5862a
owuama1 <kelechiowuama24@gmail.com>
2024-06-19 15:11:47 +0000

    betty wahala

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -8,35 +8,49 @@
  * @b: Pointer to the second deck_node_t pointer
  *
  * Return: Negative value if a should come before b, positive value if b should
- *         come before a, and zero if they are equal
+ *		   come before a, and zero if they are equal
  */
 int compare_cards(const void *a, const void *b)
 {
-    const deck_node_t *node_a = *(const deck_node_t **)a;
-    const deck_node_t *node_b = *(const deck_node_t **)b;
+	const deck_node_t *node_a = *(const deck_node_t **)a;
+	const deck_node_t *node_b = *(const deck_node_t **)b;
 
-    int value_a = 0;
-    int value_b = 0;
-    int i;
+	int value_a = 0;
+	int value_b = 0;
+	int i;
 
-    /* If kinds are the same, compare by value */
-    static const char *values[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+	static const char *const values[] = {
+		"Ace",	  /* Index 0 */
+		"2",	  /* Index 1 */
+		"3",	  /* Index 2 */
+		"4",	  /* Index 3 */
+		"5",	  /* Index 4 */
+		"6",	  /* Index 5 */
+		"7",	  /* Index 6 */
+		"8",	  /* Index 7 */
+		"9",	  /* Index 8 */
+		"10",	  /* Index 9 */
+		"Jack",   /* Index 10 */
+		"Queen",  /* Index 11 */
+		"King"	  /* Index 12 */
+	};
 
-    /* Compare by kind first */
-    if (node_a->card->kind != node_b->card->kind)
-        return node_a->card->kind - node_b->card->kind;
+
+	/* Compare by kind first */
+	if (node_a->card->kind != node_b->card->kind)
+		return (node_a->card->kind - node_b->card->kind);
 
 
-    for (i = 0; i < 13; ++i) {
-        if (strcmp(node_a->card->value, values[i]) == 0) {
-            value_a = i;
-        }
-        if (strcmp(node_b->card->value, values[i]) == 0) {
-            value_b = i;
-        }
-    }
-
-    return value_a - value_b;
+	for (i = 0; i < 13; ++i)
+	{
+		if (strcmp(node_a->card->value, values[i]) == 0)
+		{
+			value_a = i;
+		}
+		if (strcmp(node_b->card->value, values[i]) == 0)
+			value_b = i;
+	}
+	return (value_a - value_b);
 }
 
 /**
@@ -45,43 +59,43 @@ int compare_cards(const void *a, const void *b)
  */
 void sort_deck(deck_node_t **deck)
 {
-    size_t count = 0, i;
-    deck_node_t *current;
-    deck_node_t **array;
+	size_t count = 0, i;
+	deck_node_t *current;
+	deck_node_t **array;
 
-    /* Count number of nodes */
-    current = *deck;
-    while (current)
-    {
-        count++;
-        current = current->next;
-    }
+	/* Count number of nodes */
+	current = *deck;
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
 
-    /* Allocate array of pointers */
-    array = malloc(count * sizeof(*array));
-    if (array == NULL)
-        return;
+	/* Allocate array of pointers */
+	array = malloc(count * sizeof(*array));
+	if (array == NULL)
+		return;
 
-    /* Populate array with pointers to nodes */
-    i = 0;
-    current = *deck;
-    while (current)
-    {
-        array[i++] = current;
-        current = current->next;
-    }
+	/* Populate array with pointers to nodes */
+	i = 0;
+	current = *deck;
+	while (current)
+	{
+		array[i++] = current;
+		current = current->next;
+	}
 
-    /* Sort array of pointers */
-    qsort(array, count, sizeof(*array), compare_cards);
+	/* Sort array of pointers */
+	qsort(array, count, sizeof(*array), compare_cards);
 
-    /* Relink nodes based on sorted array */
-    for (i = 0; i < count - 1; i++)
-    {
-        array[i]->next = array[i + 1];
-        array[i + 1]->prev = array[i];
-    }
-    array[count - 1]->next = NULL;
-    *deck = array[0];
+	/* Relink nodes based on sorted array */
+	for (i = 0; i < count - 1; i++)
+	{
+		array[i]->next = array[i + 1];
+		array[i + 1]->prev = array[i];
+	}
+	array[count - 1]->next = NULL;
+	*deck = array[0];
 
-    free(array); /* Free temporary array */
+	free(array); /* Free temporary array */
 }
